4db0fccb58f566d77146a0f722cd84d908293042
Shun Nakatani <kaicho8636@gmail.com>
2022-08-21 15:11:46 +0900

    Add comments

--- proposition.py
+++ proposition.py
@@ -1,17 +1,22 @@
 from typing import Any, TypeVar, Generic
 from collections.abc import Callable
 from abc import ABCMeta, abstractmethod
+
+
+# ∀ S T U
 S = TypeVar('S')
 T = TypeVar('T')
 U = TypeVar('U')
 
 
+# S ∨ T
 class Or(Generic[S, T], metaclass=ABCMeta):
     @abstractmethod
     def eliminate(self, left_case: Callable[[S], U], right_case: Callable[[T], U]) -> U:
         ...
 
 
+# Construct S ∨ T from S
 class Left(Or[S, T]):
     def __init__(self, left: S):
         self.left = left
@@ -20,6 +25,7 @@ class Left(Or[S, T]):
         return left_case(self.left)
 
 
+# Construct S ∨ T from T
 class Right(Or[S, T]):
     def __init__(self, right: T):
         self.right = right
@@ -28,26 +34,31 @@ class Right(Or[S, T]):
         return right_case(self.right)
 
 
+# S ∧ T
 class And(Generic[S, T]):
     def __init__(self, left: S, right: T):
         self.left = left
         self.right = right
 
 
+# S → T
 class Implies(Generic[S, T]):
     def __init__(self, mapping: Callable[[S], T]):
         self.apply = mapping
 
 
+# ⊥
 class Bottom:
     def eliminate(self) -> Any:
         return
 
 
-class Not(Implies[S, Bottom], Generic[S]):
+# ¬S
+class Not(Implies[S, Bottom]):
     pass
 
 
+# P Q R: Proposition
 class P:
     pass
 
