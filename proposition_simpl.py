a4a31cae9bf32441350671706cacc3e1e35f3ab3
Shun Nakatani <kaicho8636@gmail.com>
2022-08-21 14:41:20 +0900

    Fix comments

--- proposition_simpl.py
+++ proposition_simpl.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Tuple
+from typing import Tuple
 
 
 class P:
@@ -13,17 +13,21 @@ class R:
     pass
 
 
-def or_commutative(hpq: P | Q) -> Q | P:  # P ∨ Q → Q ∨ P
+# P ∨ Q → Q ∨ P
+def or_commutative(hpq: P | Q) -> Q | P:
     return hpq
 
 
-def and_commutative(hpq: Tuple[P, Q]) -> Tuple[Q, P]:  # P ∧ Q → Q ∧ P
+# P ∧ Q → Q ∧ P
+def and_commutative(hpq: Tuple[P, Q]) -> Tuple[Q, P]:
     return hpq[1], hpq[0]
 
 
-def or_associative(hpqr: (P | Q) | R) -> P | (Q | R):  # (P ∨ Q) ∨ R → P ∨ (Q ∨ R)
+# (P ∨ Q) ∨ R → P ∨ (Q ∨ R)
+def or_associative(hpqr: (P | Q) | R) -> P | (Q | R):
     return hpqr
 
 
-def and_associative(hpqr: Tuple[Tuple[P, Q], R]) -> Tuple[P, Tuple[Q, R]]:  # (P ∧ Q) ∧ R → P ∧ (Q ∧ R)
+# (P ∧ Q) ∧ R → P ∧ (Q ∧ R)
+def and_associative(hpqr: Tuple[Tuple[P, Q], R]) -> Tuple[P, Tuple[Q, R]]:
     return hpqr[0][0], (hpqr[0][1], hpqr[1])
